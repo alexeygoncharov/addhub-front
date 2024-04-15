3e53af23e4b58f19414216f573b6b3537088f76d
Digerido <77206237+Digerido@users.noreply.github.com>
2024-04-15 19:29:47 +0700

    add infinitescroll

--- components/sections/messages/ChatMessages.vue
+++ components/sections/messages/ChatMessages.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="messages">
+  <div
+    v-infinite-scroll="[
+      loadMessages,
+      {
+        distance: 10,
+        canLoadMore: () => {
+          return messagesStore.isLoadedMessages.length;
+        },
+      },
+    ]"
+    class="messages"
+  >
     <SectionsMessagesChatMessage
       v-for="message in messagesStore.messages"
-      :id="'message' + message._id"
       :key="message._id"
       class="item"
       :message="message"
@@ -12,18 +22,22 @@
 
 <script setup lang="ts">
 import type { Socket } from 'socket.io-client';
-
+import { vInfiniteScroll } from '@vueuse/components';
 const messagesStore = useMessagesStore();
 const nuxtApp = useNuxtApp();
 const socket = nuxtApp.$socket as Socket;
+const loaded = ref();
 
-// messagesStore.fetchMessageList({
-//   second_side: messagesStore.activeChat._id,
-//   offset: messagesStore.offset,
-// });
+async function loadMessages() {
+  messagesStore.offset += 1;
+  await messagesStore.fetchMessageList({
+    second_side: messagesStore.activeChat._id,
+    offset: messagesStore.offset,
+  });
+}
 
 socket.on('new_message', (newMessage) => {
-  console.log('new message = ', newMessage);
+  // console.log('new message = ', newMessage);
   messagesStore.messages.push(newMessage);
 });
 </script>
