de3d556d38cb40568cc72cf54b4a4f257e99cfbf
Digerido <77206237+Digerido@users.noreply.github.com>
2024-04-26 19:28:46 +0700

    fix error

--- components/sections/messages/chatContent.vue
+++ components/sections/messages/chatContent.vue
@@ -103,12 +103,8 @@
   </div>
 </template>
 <script setup lang="ts">
-import type { Socket } from 'socket.io-client';
 const messagesStore = useMessagesStore();
-const userStore = useUserStore();
 const message = ref({ recipient: '', text: '' });
-const nuxtApp = useNuxtApp();
-const socket = nuxtApp.$socket as Socket;
 
 function sendMessage() {
   const activeChat = messagesStore.getRespondent(messagesStore.activeChat);
@@ -118,39 +114,4 @@ function sendMessage() {
     message.value = { recipient: '', text: '' };
   }
 }
-
-socket.on('new_message', (data) => {
-  if (!isFromExistingChat(data.chat._id)) {
-    messagesStore.chats.unshift(data.chat);
-  } else {
-    updateExistingChat(data.chat);
-  }
-
-  messagesStore.messages.unshift(data.newMessage);
-
-  function isFromExistingChat(chatId: string) {
-    return messagesStore.chats.some((chat) => chat._id === chatId);
-  }
-
-  function updateExistingChat(newChatData: any) {
-    // Remove outdated chat data and add updated one
-    messagesStore.chats = messagesStore.chats.filter(
-      (chat) => chat._id !== newChatData._id,
-    );
-    messagesStore.chats.unshift(newChatData);
-  }
-});
-
-socket.on('delete_chat', (deletedChat) => {
-  messagesStore.chats.filter((item) => item._id !== deletedChat.id);
-});
-
-socket.on('update_message', (updatedMessage) => {
-  messagesStore.messages.map((item) => {
-    if (item._id === updatedMessage.id) {
-      return (item.seen = true);
-    }
-    return item;
-  });
-});
 </script>
