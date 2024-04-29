5238f41b1f564e0db22abfb9ad90d83425ca4433
Digerido <77206237+Digerido@users.noreply.github.com>
2024-04-29 15:07:07 +0700

    fix

--- components/sections/messages/MessageMedia.vue
+++ components/sections/messages/MessageMedia.vue
@@ -3,7 +3,9 @@
     <div class="file-item">
       <!--<div class="file-item__title">Техническое_задание_для</div>-->
       <div class="file-item__bottom">
-        <a :href="baseUrl() + files[0]" target="_blank" class="file-item__btn">Скачать</a>
+        <a :href="baseUrl() + files[0]" target="_blank" class="file-item__btn"
+          >Скачать</a
+        >
         <!--<div class="file-item__weight">255кб</div>-->
       </div>
     </div>
